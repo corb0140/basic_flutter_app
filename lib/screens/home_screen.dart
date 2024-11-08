247335718b4be2620e32cb3b8094c77d8d6453dc
Mark Corbin <markpc1608@gmail.com>
2024-11-08 00:47:56 -0500

    added horizontal list view to vertical list view. added photos to horizontal list view

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,18 +6,43 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Basic Flutter App'),
-      ),
-      body: Center(
-          child: Text(
-        "Testing fonts",
-        style: TextStyle(
-            fontFamily: 'Montserrat',
-            fontWeight: FontWeight.bold,
-            // fontStyle: FontStyle.italic,
-            fontSize: 30),
-      )),
-    );
+        appBar: AppBar(
+          title: const Text('Basic Flutter App'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              // horizontal list view
+              SizedBox(
+                height: 200,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Image(
+                        image: AssetImage('assets/images/mercedes.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Image(
+                        image: AssetImage('assets/images/rolls-royce.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Image(
+                        image: AssetImage('assets/images/porsche.jpg'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
